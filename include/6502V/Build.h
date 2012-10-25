fcdc4b77e33f6a57145b9dd802ea0469d1573618
David H. Kristensen <dhankri@gmail.com>
2012-10-25 12:14:50 +0200

    Fixed includes

--- include/6502V/Build.h
+++ include/6502V/Build.h
@@ -1,8 +1,9 @@
 #ifndef BUILDTYPES_HEADER
 #define BUILDTYPES_HEADER
 #define FANCY_STARTMESG 1
-#define DEBUG 0
-#define CPS 10
+#define DEBUG 1
+#undef DEBUG
+#define CPS 1000
 #define DOES_WAIT 1
 #define VMAJOR 0
 #define VMINOR 10
